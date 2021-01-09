0bb85b5885f8b417a9795cee4b18d49573defc1d
jdhofmann <justindhofmann@gmail.com>
2021-01-08 22:30:59 -0500

    working out range equation

--- js/script.js
+++ js/script.js
@@ -7,16 +7,17 @@ const submitColors = (e) => {
     let red = document.querySelector("#red").value;
     let green = document.querySelector("#green").value
     let blue = document.querySelector("#blue").value
-
-    clickFunction(red, green, blue)
+    let range = document.querySelector("#range").value
+    console.log(range)
+    clickFunction(red, green, blue, range)
 }
 
 let submit = document.querySelector("form")
 submit.addEventListener("submit", submitColors)
 
 
-function clickFunction(red, green, blue){
-    let randR = Math.floor(Math.random() * (red - 27) + 27) + 1;
+function clickFunction(red, green, blue, range){
+    let randR = Math.floor(Math.random() * (((red + range/2)) - (red - range/2)) + red - range/2) + 1;
     let randG = Math.floor(Math.random() * (green - 27) + 27) + 1;
     let randB = Math.floor(Math.random() * (blue - 27) + 27) + 1;
     console.log(
@@ -24,3 +25,12 @@ function clickFunction(red, green, blue){
     )
     background.style.backgroundColor= "rgb(" +randR+ "," +randG+ "," +randB+ ")";
 }
+
+// red = 150
+// range = 25
+// 125 < red > 175
+// 
+// Math.random() * (max - min) + min;
+// Math.random()  * ( 175 - 125 ) + 125
+// max = red + range/2
+// min = red - range/2
