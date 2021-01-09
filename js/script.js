9967bf53fe40c26cd29c6c3bd1b2c8743658e24c
jdhofmann <justindhofmann@gmail.com>
2021-01-08 21:57:17 -0500

    css and cleanup

--- js/script.js
+++ js/script.js
@@ -6,21 +6,19 @@ const submitColors = (e) => {
     e.preventDefault();
     let red = document.querySelector("#red").value;
     let green = document.querySelector("#green").value
+    let blue = document.querySelector("#blue").value
 
-    clickFunction(red, green)
+    clickFunction(red, green, blue)
 }
-
-
 
 let submit = document.querySelector("form")
 submit.addEventListener("submit", submitColors)
-// button.addEventListener("click", clickFunction);
 
 
-function clickFunction(red, green){
+function clickFunction(red, green, blue){
     let randR = Math.floor(Math.random() * (red - 27) + 27) + 1;
     let randG = Math.floor(Math.random() * (green - 27) + 27) + 1;
-    let randB = Math.floor(Math.random() * (255 - 0) + 0) + 1;
+    let randB = Math.floor(Math.random() * (blue - 27) + 27) + 1;
     console.log(
         `rgb(${randR},${randG},${randB})`
     )
