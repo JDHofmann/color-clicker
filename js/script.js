e3a30429378ec3f0010500c1ed08b1a850e33641
jdhofmann <justindhofmann@gmail.com>
2021-01-08 22:44:56 -0500

    equation is there

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ let background = document.querySelector('.container');
 
 const submitColors = (e) => {
     e.preventDefault();
-    let red = document.querySelector("#red").value;
-    let green = document.querySelector("#green").value
-    let blue = document.querySelector("#blue").value
-    let range = document.querySelector("#range").value
+    let red = parseInt(document.querySelector("#red").value);
+    let green = parseInt(document.querySelector("#green").value);
+    let blue = parseInt(document.querySelector("#blue").value);
+    let range = parseInt(document.querySelector("#range").value);
     console.log(range)
     clickFunction(red, green, blue, range)
 }
@@ -16,9 +16,21 @@ let submit = document.querySelector("form")
 submit.addEventListener("submit", submitColors)
 
 
+const max = (color, range) => {
+    return color + range/2
+}
+
+const min = (color, range) => {
+    return color - range/2
+}
+
 function clickFunction(red, green, blue, range){
-    let randR = Math.floor(Math.random() * (((red + range/2)) - (red - range/2)) + red - range/2) + 1;
-    let randG = Math.floor(Math.random() * (green - 27) + 27) + 1;
+
+    console.log(max(red, range))
+
+    let randR = Math.floor(Math.random() * (max(red, range) - min(red,range)) + min(red,range));
+
+    let randG = Math.floor(Math.random() * (max(green, range) - 27) + 27) + 1;
     let randB = Math.floor(Math.random() * (blue - 27) + 27) + 1;
     console.log(
         `rgb(${randR},${randG},${randB})`
@@ -30,7 +42,7 @@ function clickFunction(red, green, blue, range){
 // range = 25
 // 125 < red > 175
 // 
-// Math.random() * (max - min) + min;
+// Math.random() * (max() - min()) + min();
 // Math.random()  * ( 175 - 125 ) + 125
 // max = red + range/2
 // min = red - range/2
